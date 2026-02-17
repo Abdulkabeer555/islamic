ff616a2d7ef89ec2d0f77ceb983961e87bdbccca
Abdulkabeer555 <akgct123@gmail.com>
2026-02-17 19:59:29 +0500

    islamic

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.nav-hover .nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        if (window.innerWidth < 992) {
+            e.preventDefault();
+            const parent = this.parentElement;
+            
+            document.querySelectorAll('.nav-item.dropdown').forEach(item => {
+                if (item !== parent) item.classList.remove('show');
+            });
+
+            parent.classList.toggle('show');
+        }
+    });
+});
