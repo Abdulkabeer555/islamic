217764b266272ede26ca0de60f174fa6c3341a14
Abdulkabeer555 <akgct123@gmail.com>
2026-02-21 15:31:33 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,3 @@ document.querySelectorAll('.nav-hover .nav-link').forEach(link => {
         }
     });
 });
-
-window.shareLinkedIn = function() {
-    const url = encodeURIComponent(window.location.href);
-    const appUrl = "linkedin://shareArticle?mini=true&url=" + url;
-    const webUrl = "https://www.linkedin.com/sharing/share-offsite/?url=" + url;
-
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-    if (isMobile) {
-        window.location.href = appUrl;
-        setTimeout(function() {
-            window.open(webUrl, '_blank');
-        }, 600);
-    } else {
-        window.open(webUrl, '_blank', 'width=600,height=400');
-    }
-};
